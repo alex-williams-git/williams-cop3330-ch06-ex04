ae6a00ccc03fddd6cd9562133ae377a5b205f351
Alexander Williams <alexander.williams@knights.ucf.edu>
2021-11-29 00:07:50 -0500

    first commit

---
+++ App.cpp
@@ -0,0 +1,80 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Alexander Williams
+ */
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Name_value{
+    // getters and setters and constructor
+    public:
+    Name_value(string string, int value){
+        setString(string);
+        setValue(value);
+    }
+    void setString(string s){
+        name = s;
+    }
+    string getString(){
+        return name;
+    }
+    void setValue(int v){
+        score = v;
+    }
+    int getValue(){
+        return score;
+    }
+
+    private:
+    string name;
+    int score;
+};
+
+// function to check vector for repeat names
+void checkVector(vector<Name_value> vector, string string){
+    for(int i = 0; i < vector.size(); i++){
+        if(string == vector[i].getString()){
+            printf("Name entered twice.");
+            exit(-1);
+        }
+    }
+}
+
+// function to print the vector out
+void printVector(vector <Name_value> vector){
+    for(int i = 0; i < vector.size(); i++){
+        cout << vector[i].getString() << " ";
+        cout << vector[i].getValue() << endl;
+    }
+}
+
+int main(){
+    vector<Name_value> valueArray;
+    string tempString = "n/a";
+    int tempInt = -1;
+
+    // while the user hasn't entered NoName and 0, add the input to the vector
+    while((tempString != "NoName") && (tempInt != 0)){
+        // scanning input
+        cin >> tempString;
+        scanf("%d", &tempInt);
+
+        // checking for repeated names
+        checkVector(valueArray, tempString);
+
+        // add to the vector if the input is not NoName and 0
+        if((tempString != "NoName") && (tempInt != 0)){
+            Name_value tempClass(tempString, tempInt);
+            valueArray.push_back(tempClass);
+        }
+    }
+
+    // print vector
+    printVector(valueArray);
+}
